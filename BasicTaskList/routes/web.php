ce9191ebaf57cafc95e225d3bec701a6279558c1
Aarthna Maheshwari <mailaarthna@gmail.com>
2019-07-27 13:24:35 +0530

    Updated all the files
    Updated with complete source code

--- BasicTaskList/routes/web.php
+++ BasicTaskList/routes/web.php
@@ -12,11 +12,20 @@
 */
 
 Route::get('/', function () {
-    return view('tasks');
+	$tasks=\App\Task::orderBy('created_at','asc')->get();
+    return view('tasks',['tasks'=>$tasks]);
 });
-Route::post('/task',function(){
-
+Route::post('/task',function(Illuminate\Http\Request $request){
+	$validator=Validator::make($request->all(),['name'=>'required|max:255']);
+	if ($validator->fails()) {
+		return redirect('/')->withInput()->withErrors($validator);
+	}
+	$task=new \App\Task;
+	$task->name=$request->name;
+	$task->save();
+	return redirect('/');
 });
-Route::delete('/task/{id}',function(){
-
+Route::delete('/task/{id}',function($id){
+	\App\Task::findOrFail($id)->delete();
+	return redirect('/');
 });
